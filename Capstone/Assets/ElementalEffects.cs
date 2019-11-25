497c8367699947edbbd6bd51ce2508686dd270fc
greenej10 <jacobgreene.rfp@gmail.com>
2019-11-25 17:23:27 -0500

    Redo Lift/Wind

--- Capstone/Assets/ElementalEffects.cs
+++ Capstone/Assets/ElementalEffects.cs
@@ -8,20 +8,28 @@ public class ElementalEffects : MonoBehaviour
     public float liftForce = 10;
     private Rigidbody2D rb;
     private string type;
+    private float angle;
+    public Transform p1;
+    public Transform p2;
+    private AreaEffector2D areaEffector;
+    
        
     // Start is called before the first frame update
     void Start()
     {
         type = this.tag;
+        areaEffector = gameObject.GetComponent<AreaEffector2D>();
+        angle = Mathf.Atan2(p2.position.y - p1.position.y, p2.position.x - p1.position.x) * 180 / Mathf.PI;
+        areaEffector.forceAngle = angle;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        
     }
 
-    void OnTriggerStay2D(Collider2D other)
+   void OnTriggerStay2D(Collider2D other)
     {
 
         Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
@@ -31,6 +39,7 @@ public class ElementalEffects : MonoBehaviour
         {
             Debug.Log("Trigger Occured");
             rb.velocity = Vector2.up * liftForce;
-        }    
+        }
+
     }
 }

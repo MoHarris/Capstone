77c8a00f92993698d11938a7e7b25305c25bf7ec
greenej10 <jacobgreene.rfp@gmail.com>
2019-12-07 19:06:27 -0500

    lvl 3 update 1

---
+++ Capstone/Assets/MovingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    //public GameObject player;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.parent = gameObject.transform;
+        }
+              
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.parent = null;
+        }
+        
+    }
+    
+}
